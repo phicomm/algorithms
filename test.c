84d051420e3788eef06704e3c13ab54b9f2441c6
phicomm <phicomm@outlook.com>
2014-10-15 19:56:54 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 /* test */
 
 /*
- * 题目：求1+2+..+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字以及条件判断语句(A?B:C)。
+ * 求1+2+..+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字以及条件判断语句(A?B:C)。
  * 解法1：利用递归、&&运算符属性。
  */
 int add1(int n)
@@ -13,7 +13,7 @@ int add1(int n)
 }
 
 /*
- * 题目：求1+2+..+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字以及条件判断语句(A?B:C)。
+ * 求1+2+..+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字以及条件判断语句(A?B:C)。
  * 解法2：利用递归、&&运算符、静态变量属性。
  */
 int add2(int n)
@@ -22,4 +22,37 @@ int add2(int n)
 
   n && add2(n-1);
   return (sum += n);
+}
+
+
+/*
+ * 判断该数组是不是某二元查找树的后序遍历的结果。
+ * 输入: 数组a[low, high]
+ */
+int ispostorder(int a[], int low, int high)
+{
+  int i, middle;
+  int ret = TRUE;
+
+  for (i = low; i < high && a[i] < a[high]; i++)
+    ;
+
+  middle = i;
+
+  for (; i < high; i++)
+  {
+    if (a[i] < a[high])
+      return FALSE;
+  }
+
+  if (low < middle - 1)
+    ret = ispostorder(a, low, middle - 1);
+
+  if (FALSE == ret)
+    return ret;
+
+  if (middle < high - 1)
+    ret = ispostorder(a, middle, high - 1);
+
+  return ret;
 }
